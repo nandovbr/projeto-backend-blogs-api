39fd43fed08d38f688a2d8887e92100586a3f464
nandovbr <nandovbr15@hotmail.com>
2022-09-05 18:36:53 -0300

    adiciona requisito 10

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -11,5 +11,9 @@ module.exports = (sequelize, DataTypes) => {
     tableName: 'Users',
   });
 
+User.associate = (models) => {
+  User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'posts' });
+};
+
   return User;
 };
