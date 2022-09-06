e42addbc4413b0edf557a0f0b5685806b1a57ede
nandovbr <nandovbr15@hotmail.com>
2022-09-06 11:12:39 -0300

    ajusta requisito 13

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -3,7 +3,8 @@ const { BlogPost, User, Category } = require('../database/models');
 const getPosts = async (_req, res) => {
   const posts = await BlogPost.findAll({
     include: [{ model: User, as: 'user', attributes: { exclude: ['password'] } },
-    { model: Category, as: 'categories', through: { attributes: [] } }],
+    { model: Category, as: 'categories',
+    }],
   });
 
   res.status(200).json(posts);
